214b27b526646c4b1cd8d74f427c37983219894e
KaanOzen-mF <kaanozn98@gmail.com>
2022-09-23 15:18:12 +0300

    Porfolio web page is nearly done

---
+++ index.js
@@ -0,0 +1,46 @@
+let home = document.getElementById("home");
+let navbar = document.getElementById("main-nav");
+
+let navPos = navbar.getBoundingClientRect().top;
+
+window.addEventListener("scroll", (e) => {
+	let scrollPos = window.scrollY;
+	if (scrollPos > navPos) {
+		navbar.classList.add("sticky");
+		home.classList.add("navbarOffsetMargin");
+	} else {
+		navbar.classList.remove("sticky");
+		home.classList.remove("navbarOffsetMargin");
+	}
+});
+
+const lines = document.querySelectorAll(".line");
+const titles = document.querySelectorAll(".section-title");
+
+window.addEventListener("scroll", check_animation);
+
+check_animation();
+
+function check_animation() {
+	const trigger = (window.innerHeight / 6) * 4;
+
+	lines.forEach((line) => {
+		const top = line.getBoundingClientRect().top;
+
+		if (trigger > top) {
+			line.classList.add("line-animation");
+		} else {
+			line.classList.remove("line-animation");
+		}
+	});
+
+	titles.forEach((title) => {
+		const top = title.getBoundingClientRect().top;
+
+		if (trigger > top) {
+			title.classList.add("section-title-animation");
+		} else {
+			title.classList.remove("section-title-animation");
+		}
+	});
+}
