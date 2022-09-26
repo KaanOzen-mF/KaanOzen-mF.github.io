caef93faa8857e6d8dee020141c86c05d0ba5f98
KaanOzen-mF <kaanozn98@gmail.com>
2022-09-26 10:49:07 +0300

    PortfolioWebPage is done

--- index.js
+++ index.js
@@ -50,3 +50,33 @@ var pix = document.getElementsByClassName("pixel");
 for (var i = 0; i < pix.length; i++) {
 	pix[i].style.animationDelay = Math.ceil(Math.random() * 5000) + "ms";
 }
+
+const skillsSection = document.getElementById("skills-section");
+
+const progressBars = document.querySelectorAll(".progress-bar");
+
+function showProgress() {
+	progressBars.forEach((progressBar) => {
+		const value = progressBar.dataset.progress;
+		progressBar.style.opacity = 1;
+		progressBar.style.width = `${value}%`;
+	});
+}
+
+function hideProgress() {
+	progressBars.forEach((p) => {
+		p.style.opacity = 0;
+		p.style.width = 0;
+	});
+}
+
+window.addEventListener("scroll", () => {
+	const sectionPos = skillsSection.getBoundingClientRect().top;
+	const screenPos = window.innerHeight / 2;
+
+	if (sectionPos < screenPos) {
+		showProgress();
+	} else {
+		hideProgress();
+	}
+});
