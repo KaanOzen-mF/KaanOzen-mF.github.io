5bc314f3093a3d21ef98468810628538a8cce161
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-06 12:19:46 +0300

    add project data file

---
+++ data.js
@@ -0,0 +1,15 @@
+export const data = {
+  project1: {
+    projectName: "Movie Search App with JS",
+    github:
+      "https://github.com/KaanOzen-mF/JS-Api-Projects/tree/master/movieList",
+    live: "https://movieelist.netlify.app/",
+  },
+  project6: {
+    projectName: "Color Scheme Generator",
+    github:
+      "https://github.com/KaanOzen-mF/JS-Api-Projects/tree/master/colorSchemeGenerator",
+    live: "https://colourschemegenerator.netlify.app/",
+  },
+  // Add other projects as needed
+};
