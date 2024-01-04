b81d3e02d9ed9ea0aa956f4a9f677fdd82d69b52
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-04 17:56:11 +0300

    Todo project data add

--- data.js
+++ data.js
@@ -5,6 +5,11 @@ export const data = {
       "https://github.com/KaanOzen-mF/JS-Api-Projects/tree/master/movieList",
     live: "https://movieelist.netlify.app/",
   },
+  project2: {
+    projectName: "Todo App with React",
+    github: "https://github.com/KaanOzen-mF/to-do-app",
+    live: "https://to-do-app-flax-iota.vercel.app/todo",
+  },
   project6: {
     projectName: "Color Scheme Generator",
     github:
